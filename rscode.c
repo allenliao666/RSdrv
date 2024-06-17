b7ff17a5dab4b6272bae72474e39658b8b38bae9
allenliao666 <liao4792856@gmail.com>
2024-06-17 20:40:05 +0800

    FIX TYPO AND FORMAT

--- rscode.c
+++ rscode.c
@@ -1,19 +1,22 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/reed_solomon.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("allenliao666");
+MODULE_DESCRIPTION("A Reed-Solomon Test module");
 
-static int __init chrdev_init(void)
+static int __init rsdrv_init(void)
 {
-	printk(KERN_INFO "rscode test module init\n");
-	return 0;
+    printk(KERN_INFO "rscode test module init\n");
+    return 0;
 }
 
-static void __exit chrdev_exit(void)
+static void __exit rsdrv_exit(void)
 {
-	printk(KERN_INFO "escode test module exit\n");
-	return;
+    printk(KERN_INFO "rscode test module exit\n");
+    return;
 }
 
 module_init(chrdev_init);
