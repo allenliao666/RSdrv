550b151473cda08976a092308afa74ef722156c2
allenliao666 <liao4792856@gmail.com>
2024-06-18 13:16:53 +0800

    INCLUDE REED_SOLOMON HEADER FILE include reed_solomon header file and initialize a rs_control structure.

--- rscode.c
+++ rscode.c
@@ -1,6 +1,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/reed_solomon.h>
+#include <linux/rslib.h>
 
 
 MODULE_LICENSE("GPL");
@@ -9,7 +9,19 @@ MODULE_DESCRIPTION("A Reed-Solomon Test module");
 
 static int __init rsdrv_init(void)
 {
-    printk(KERN_INFO "rscode test module init\n");
+    struct rs_control *rs;
+    unsigned char data[255];
+    unsigned char parity[32];
+    int eras_pos[32];
+    int i;
+
+    //initialize a rs_control structure
+    rs = init_rs(8, 0x11d, 0, 1, 32);
+    if(!rs) {
+        printk(KERN_ERR "failed to initializw rs_control structure\n");
+	return -ENOMEM;
+    }    
+    printk(KERN_INFO "rscode test module initialize\n");
     return 0;
 }
 
@@ -19,5 +31,5 @@ static void __exit rsdrv_exit(void)
     return;
 }
 
-module_init(chrdev_init);
-module_exit(chrdev_exit);
+module_init(rsdrv_init);
+module_exit(rsdrv_exit);
